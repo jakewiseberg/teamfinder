ff8888238c114e1080463fc74e58dcc6d631ab49
Jake Wiseberg <jakewiseberg@gmail.com>
2016-05-21 21:25:15 -0400

    added backend

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+  <html lang = "en">
+    <head>
+      <meta charset = "utf-8">
+      <!--char set -->
+      <meta name = "viewport" content = "width=device-width, initial-scale=1">
+      <!--view settings optimized for mobile browsers-->
+      <title>Hackathon team Generator</title>
+      <link rel=icon href="favicon.ico">
+      <style>
+        .container {
+          max-width: 50%;
+          margin-left: auto;
+          margin-right: auto;
+          padding: 1em;
+          text-align: center;
+        }
+      </style>
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      <script>
+      </script>
+    </head>
+    <body>
+      <nav class = "navbar navbar-default">
+        <div class = "container-fluid">
+          <div class = "navbar-header">
+            <a class = "navbar-brand" href = "#"><img class = "img-responsive" src = "images/mlh_logo.png" style = "width: 80px; display: block; margin-top: auto; margin-bottom: auto; position: fixed; top: 1%; left: 1;"></a>
+            <!--trouble point-->
+          </div>
+          <ul class = "nav navbar-nav navbar-right">
+            <li><a href = "https://mlh.io/about">About MLH</a></li>
+            <li><a href = "login.php">Log in</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class = "container">
+        <img class = "img-responsive" src = "images/mlh_logo.png" style = "margin-top: 3em;">
+        <h1>Hackathon Team Generator</h1>
+        <p>The <b>MLH Hackathon Team Generator</b> seeks to facilitate team-making for those who can't make a team before the hackathon starts. While most hackathons have a session dedicated to creating new teams, this takes much time away from going to workshops and hacking. This is a problem our team members ran into that many other hackers here have run into. So, we introduce to you this unique tool to allow you to find new members!</p>
+        <a href="register.php" class="btn btn-info" role="button">Sign Up</a>
+      </div>
+    </body>
+  </html>
