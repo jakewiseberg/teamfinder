e7b7a8f435c6949178625d858144a4008c012a61
BuildTools <unconfigured@null.spigotmc.org>
2016-05-21 21:10:34 -0400

    Added a javascript for processing users. Under development. Contains Person class, compareTo function, and getDataFromTypeForm function.

---
+++ ProcessData.js
@@ -0,0 +1,24 @@
+
+class Person{
+  constructor(numberOfHackathons, favLanguage, interests, numbOfMembers, favFood, sleep){
+      this.numbOfHackathons = numberOfHackathons;
+      this.favLanguage = favLanguage;
+      this.interests = interests;
+      this.numbOfMembers = numbOfMembers;
+      this.favFood = favFood;
+      this.sleep = sleep;
+      this.points = 0;
+
+  }
+}
+
+function compareTo(a, b) {
+  if(!((a instanceof Person) || (b instanceof Person))){
+    return -1;
+  }
+function getDataFromTypeForm(){
+
+  alert($.get("https://api.typeform.com/v1/form/r4h28z?key=61977fb4781ae43288278d820089583bf7100073"));
+}
+
+}
